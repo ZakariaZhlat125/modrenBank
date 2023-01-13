0635a9801706cd0ea9a78a0fbfc0264828f28783
Zakaria Zhlat <zakooorr1567@gmail.com>
2023-01-13 11:41:32 +0200

    FirstCommit

---
+++ src/components/index.js
@@ -0,0 +1,24 @@
+import NavBar from "./NavBar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import CTA from "./CTA";
+import Business from "./Business";
+import Button from "./Button";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Clients from "./Clients";
+import Hero from "./Hero";
+export {
+  NavBar,
+  Billing,
+  CardDeal,
+  CTA,
+  Hero,
+  Business,
+  Button,
+  Stats,
+  Footer,
+  Testimonials,
+  Clients,
+};
